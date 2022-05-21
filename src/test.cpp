aebfe15d8a5e60f0906498ba4dc6676f6b44f05e
Evgeniy Osipov <Evgeniy.Osipov@itiviti.com>
2022-05-21 17:46:07 +0200

    fix reference values up

--- src/test.cpp
+++ src/test.cpp
@@ -13,6 +13,7 @@ class TrackerTest : public ::testing::Test {
         }
 
         bool check(const Cartesian & val, const Cartesian & reference) {
+            // std::cout << "Test: reference value " << reference << ", current value " << val << "\n";
             auto dx = val.x - reference.x;
             auto dy = val.y - reference.y;
             auto dist = std::sqrt(dx * dx + dy * dy);
@@ -31,7 +32,7 @@ TYPED_TEST(TrackerTest, EarthPosition)
     this->load_data("test/etc/planets.txt");
 
     Track t = this->m_tracker->track("Earth", 1000, 1);
-    ASSERT_TRUE(this->check(t.back(), {1.49599e+11, 2.98e+07}));
+    ASSERT_TRUE(this->check(t.back(), {1.496e+11, 2.98e+07}));
 }
 
 TYPED_TEST(TrackerTest, SunPosition)
@@ -39,7 +40,7 @@ TYPED_TEST(TrackerTest, SunPosition)
     this->load_data("test/etc/planets.txt");
 
     Track t = this->m_tracker->track("Sun", 1000, 1);
-    ASSERT_TRUE(this->check(t.back(), {8.84964, 0.000753268}));
+    ASSERT_TRUE(this->check(t.back(), {0.0264958, 3.00701e-06}));
 }
 
 TYPED_TEST(TrackerTest, VenusPosition)
@@ -47,7 +48,7 @@ TYPED_TEST(TrackerTest, VenusPosition)
     this->load_data("test/etc/planets.txt");
 
     Track t = this->m_tracker->track("Venus", 1000, 1);
-    ASSERT_TRUE(this->check(t.back(), {1.08198e+11, 3.49998e+07}));
+    ASSERT_TRUE(this->check(t.back(), {1.082e+11, 3.5e+07}));
 }
 
 TYPED_TEST(TrackerTest, EarthPosition2)
@@ -55,7 +56,7 @@ TYPED_TEST(TrackerTest, EarthPosition2)
     this->load_data("test/etc/planets2.txt");
 
     Track t = this->m_tracker->track("Earth", 1000, 1);
-    ASSERT_TRUE(this->check(t.back(), {-6.19286e+10, -5.64705e+08}));
+    ASSERT_TRUE(this->check(t.back(), {-6.24983e+10, -5.65995e+08}));
 }
 
 TYPED_TEST(TrackerTest, VenusPosition2)
@@ -63,5 +64,5 @@ TYPED_TEST(TrackerTest, VenusPosition2)
     this->load_data("test/etc/planets2.txt");
 
     Track t = this->m_tracker->track("Venus", 1000, 1);
-    ASSERT_TRUE(this->check(t.back(), {5.64705e+08, -6.19286e+10}));
+    ASSERT_TRUE(this->check(t.back(), {5.65995e+08, -6.24983e+10}));
 }
